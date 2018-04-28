90fcb0b9d04a6eff6dab4ad6497a91b04a5706c9
Goinmul <21400045@handong.edu>
2018-04-29 07:24:14 +0900

    now I understood some reasons for using static + super in prob2.

--- src/main/java/edu/handong/csee/java/lab13/prob2/BookMain.java
+++ src/main/java/edu/handong/csee/java/lab13/prob2/BookMain.java
@@ -5,15 +5,14 @@ public class BookMain {
 	public static void main(String[] args) {
 		
 		Book book = new Book("1984");
-		History history = new History("Korean History","Kwon Hyun Woo");
 		Science science = new Science("Basic Physics", "Newton");
+		History history = new History("Korean History","Kwon Hyun Woo");
 		
 		book.show();
 		science.show();
 		history.show();
-
-
-	}
-
+		
+	} // you must instantiate + use 'show()' method symmetrically, or the Id will increase differently.
+	// In other words, the Id increases following the order of instantiation.
 	
 }
--- src/main/java/edu/handong/csee/java/lab13/prob3/ShapeMain.java
+++ src/main/java/edu/handong/csee/java/lab13/prob3/ShapeMain.java
@@ -4,8 +4,30 @@ public class ShapeMain {
 	
 	public static void main(String[] args) {
 		
-		double n1,n2;
+		
+		double n1,n2,n3;
 		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Enter radius : ");
+		n1 = keyboard.nextDouble();
+		
+		Circle myCircle = new Circle(n1);
+		System.out.println("Radius : " + n1);
+		myCircle.display(); // even though in the Circle Class there is no implementation for this method, you just inherited the method from superclass.
+		
+		System.out.println(""); // changing line
+		
+		
+		
+		System.out.print("Enter length : ");
+		n2 = keyboard.nextDouble();
+		System.out.print("Enter width : ");
+		n3 = keyboard.nextDouble();
+		
+		Rectangle myRec = new Rectangle(n2,n3);
+		System.out.println("Length : " + n2);
+		System.out.println("Width : " + n3);
+		myRec.display();
 	}
 
 }
